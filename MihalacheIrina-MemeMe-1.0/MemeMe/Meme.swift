622b96666a1a299521612785684c7ee24bd70a22
Mihalache Irina Mihaela <mihalacheirina88@gmail.com>
2019-12-22 21:15:56 +0200

    Add files via upload

---
+++ MihalacheIrina-MemeMe-1.0/MemeMe/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Irina on 10/06/2019.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
